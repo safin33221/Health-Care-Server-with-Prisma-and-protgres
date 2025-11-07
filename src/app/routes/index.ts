bc1453f74fc900227e8fccc3b24984beea845c3a
safin33221 <safin33221@gmail.com>
2025-11-07 21:49:56 +0600

    init project and create user model

---
+++ src/app/routes/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { userRoutes } from '../modules/user/user.route';
+
+
+const router = express.Router();
+
+const moduleRoutes = [
+    {
+        path: '/',
+        route: router
+    },
+    {
+        path: '/user',
+        route: userRoutes
+    },
+];
+
+
+
+moduleRoutes.forEach(route => router.use(route.path, route.route))
+
+export default router;
