b093c6a9c3b3468653be03602c38c03375e58b2d
safin33221 <safin33221@gmail.com>
2025-11-13 21:29:50 +0600

    Improvement error handling

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,5 @@
+
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express"
 import httpStatus from "http-status"
 
@@ -7,6 +9,21 @@ const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFun
     let success = false;
     let message = err.message || "Something went wrong!";
     let error = err;
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code == "P2002") {
+            message = "Duplicate Key Error",
+                error = err.meta
+        }
+        if (error.coe == "P1000") {
+            message = "Authentication failed against database server",
+                error = err.meta
+        }
+        if (error.coe == "P2003") {
+            message = "Foreign key constraint failed",
+                error = err.meta
+        }
+    }
 
     res.status(statusCode).json({
         success,
